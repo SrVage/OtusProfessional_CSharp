b16f966f0f0fa22afe7f0e2de7476a4e0f28ae39
Maksim_Krampit <m.krampit@pixelskystudio.com>
2026-03-12 10:54:30 +0700

    Homework done

--- TCPServer/TcpServer.cs
+++ TCPServer/TcpServer.cs
@@ -16,6 +16,9 @@ public class TcpServer : IDisposable
     private Socket _socket;
     private readonly Lock _lock = new();
     private readonly SimpleStore _simpleStore;
+    private byte[] _keyNotFoundResponse;
+    private byte[] _okResponse;
+    private byte[] _unknownCommandResponse;
 
     private bool IsRunning
     {
@@ -31,6 +34,7 @@ public class TcpServer : IDisposable
     
     public TcpServer(string ip, int port, SimpleStore simpleStore)
     {
+        InitializeStandardResponses();
         _simpleStore = simpleStore;
         if (IPAddress.TryParse(ip, out IPAddress? ipAddress))
         {
@@ -53,6 +57,7 @@ public class TcpServer : IDisposable
 
     public TcpServer(SimpleStore simpleStore)
     {
+        InitializeStandardResponses();
         _simpleStore = simpleStore;
         if (IPAddress.TryParse("127.0.0.1", out IPAddress? ipAddress))
         {
@@ -60,6 +65,13 @@ public class TcpServer : IDisposable
             _port = 8080;
             _cancellationTokenSource = new CancellationTokenSource();
         }
+    }
+
+    private void InitializeStandardResponses()
+    {
+        _keyNotFoundResponse = Encoding.UTF8.GetBytes("Key not found \r\n");
+        _okResponse = Encoding.UTF8.GetBytes("OK\r\n");
+        _unknownCommandResponse = Encoding.UTF8.GetBytes("-ERR Unknown command\r\n");
     }
 
     public async Task StartAsync()
@@ -156,6 +168,22 @@ public class TcpServer : IDisposable
                 
                 ReadOnlySpan<char> span = receivedData.AsSpan();
                 var parseResult = CommandParser.Parse(span);
+
+                switch (parseResult.Command)
+                {
+                    case "GET":
+                        ProcessGetCommand(clientSocket, parseResult);
+                        break;
+                    case "SET":
+                        ProcessSetCommand(clientSocket, parseResult);
+                        break;
+                    case "DELETE":
+                        ProcessDeleteCommand(clientSocket, parseResult);
+                        break;
+                    default:
+                        clientSocket.SendAsync(_unknownCommandResponse);
+                        break;
+                }
                 
                 Console.WriteLine("----Parse result----");
                 Console.WriteLine("Command: {0}", parseResult.Command.ToString());
@@ -201,6 +229,31 @@ public class TcpServer : IDisposable
                 Console.WriteLine($"Client disconnected from {clientEndpoint}");
             }
         }
+    }
+
+    private void ProcessDeleteCommand(Socket clientSocket, ParseCommand parseResult)
+    {
+        var key = parseResult.Key.ToString();
+        _simpleStore.Delete(key);
+        clientSocket.SendAsync(_okResponse);
+    }
+
+    private void ProcessSetCommand(Socket clientSocket, ParseCommand parseResult)
+    {
+        var key = parseResult.Key.ToString();
+        var valueBytesCount = Encoding.UTF8.GetByteCount(parseResult.Value);
+        var spanArray = ArrayPool<byte>.Shared.Rent(valueBytesCount);
+        Encoding.UTF8.GetBytes(parseResult.Value, spanArray);
+        _simpleStore.Set(key, spanArray);
+        ArrayPool<byte>.Shared.Return(spanArray);
+        clientSocket.SendAsync(_okResponse);
+    }
+
+    private void ProcessGetCommand(Socket clientSocket, ParseCommand parseResult)
+    {
+        var key = parseResult.Key.ToString();
+        var result = _simpleStore.Get(key);
+        clientSocket.SendAsync(result ?? _keyNotFoundResponse);
     }
 
     public void Dispose()
--- TcpClient/Program.cs
+++ TcpClient/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System.Buffers;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
@@ -6,7 +7,10 @@ var serverAddress = IPAddress.Loopback;
 var serverPort = 8080;
 
 await SendCommandAsync(serverAddress, serverPort, "  SET user:1 John");
+await Task.Delay(1000);
 await SendCommandAsync(serverAddress, serverPort, "  GET user:1");
+await Task.Delay(1000);
+await SendCommandAsync(serverAddress, serverPort, "  DELETE user:1");
 
 Console.WriteLine("Client stopped");
 
@@ -21,6 +25,12 @@ static async Task SendCommandAsync(IPAddress serverAddress, int serverPort, stri
         byte[] commandBytes = Encoding.UTF8.GetBytes(command + "\n");
         await clientSocket.SendAsync(commandBytes, SocketFlags.None);
         Console.WriteLine($"Send: {command}");
+        var responseArray = ArrayPool<byte>.Shared.Rent(1024);
+        await clientSocket.ReceiveAsync(responseArray, SocketFlags.None);
+        var responseText = Encoding.UTF8.GetString(responseArray, 0, responseArray.Length);
+        Console.WriteLine("Server response");
+        Console.WriteLine(responseText);
+        ArrayPool<byte>.Shared.Return(responseArray);
         clientSocket.Shutdown(SocketShutdown.Both);
     }
     catch (SocketException ex)
